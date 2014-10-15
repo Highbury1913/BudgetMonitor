d5e1c7b9ecf7f01ba1d9c6d7c53c12314e8fa863
Ronny Schulz <rschulz@funkwerk-itk.com>
2014-10-15 18:10:41 +0200

    removed twitter and github from auth services

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var crypto = require('crypto');
-var authTypes = ['github', 'twitter', 'facebook', 'google'];
+var authTypes = ['facebook', 'google'];
 
 var UserSchema = new Schema({
   name: String,
@@ -16,8 +16,7 @@ var UserSchema = new Schema({
   provider: String,
   salt: String,
   facebook: {},
-  google: {},
-  github: {}
+  google: {}
 });
 
 /**
