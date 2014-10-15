f08a524226369333688f9498dcec5847356b8662
Ronny Schulz <rschulz@funkwerk-itk.com>
2014-10-15 17:54:33 +0200

    added validators

--- server/api/budget/budget.model.js
+++ server/api/budget/budget.model.js
@@ -6,15 +6,53 @@ var mongoose = require('mongoose'),
 var BudgetSchema = new Schema({
   name: String,
   info: String,
-  owner: {
-    type: mongoose.Scheme.Types.ObjectId,
+  _owner: {
+    type: Schema.Types.ObjectId,
     ref: 'User'
     },
-  users: [{
-    userid: mongoose.Scheme.Types.ObjectId
-  }}
+  access: [{
+    _userid: Schema.Types.ObjectId
+  }],
   interval: String,
   intervaldata: [{startdate:Date, budget: Number}],
+  currencySymbol: String,
 });
+
+/**
+ * Validations
+ */
+
+// Validate budget
+BudgetSchema
+  .path('access')
+  .validate(function(access) {
+    return (access.length > 0);
+  }, 'Needs at least one user in access list');
+
+
+var validatePresenceOf = function(value) {
+  return value && value.length;
+};
+
+var validateObjectId = function(value) {
+  return value;
+};
+
+
+/**
+ * Pre-save hook
+ */
+BudgetSchema
+  .pre('save', function(next) {
+    if (!this.isNew) return next();
+
+    if (!validatePresenceOf(this.access))
+      next(new Error('Missing access list.'));
+    else if (!validateObjectId(this._owner))
+      next(new Error('Missing owner'));
+    else
+      next();
+  });
+
 
 module.exports = mongoose.model('Budget', BudgetSchema);
